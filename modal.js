265d161aec799231929a90b506ebadd4020086f8
JK <jk.floods@gmail.com>
2022-06-22 15:13:45 +0200

    Header | Upgrate to CSS3 + Sass

--- modal.js
+++ modal.js
@@ -1,9 +1,9 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
+  var x = document.querySelector(".nav");
+  if (x.className === "nav") {
     x.className += " responsive";
   } else {
-    x.className = "topnav";
+    x.className = "nav";
   }
 }
 
